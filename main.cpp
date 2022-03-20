a731b1cfd1ac3a4c18c816c2777f8e36b50e2b2e
Raph2111 <81633909+Raph2111@users.noreply.github.com>
2022-03-20 18:39:39 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "Project.hpp"
+#include "Projet.hpp"
 
 using namespace std;
 
 int main()
 {
-    Maillage M(0,14.,0,14.,100,100);
-    M.affiche();
+
+    Maillage M(0,14.,0,14.,10,10);
+    Point P(0,2);
+    vecteur VV =V(P);
+    //matrice_nonsym BB = matB(M.sommets, M.numelts);
+    //cout<<BB<<endl;
+    vecteur PP = resolution_1(0.1,5,VV,M.sommets,M.numelts);
+    cout<<PP<<endl;
     return 0;
 }
