b3557a691e7d61182ed2fe2094533a7e8c29db59
Raph2111 <81633909+Raph2111@users.noreply.github.com>
2022-03-21 19:13:45 +0100

    Add files via upload

--- Rendu/main.cpp
+++ Rendu/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include "Project.hpp"
+#include "Projet.hpp"
 
 using namespace std;
 
 int main()
 {
 
-    Maillage M(0,14.,0,14.,100,100);
+    Maillage M(0,14.,0,14.,30,30);
     M.savecoord("Coorneu.txt");
     M.savenumtri("Numtri.txt");
-    Point P(0,2);
-    vecteur VV =V(P);
-    vecteur PP = resolution_1(0.1,50,M.sommets,M.numelts);
+    vecteur PP = resolution_1(0.01,10,M.sommets,M.numelts);
     cout<<"résultat d'ordre 1:"<<PP<<endl;
-    vecteur PP2 = resolution_2(0.1,50,M.sommets,M.numelts);
+    vecteur PP2 = resolution_2(0.001,25,M.sommets,M.numelts);
     cout<<"résultat d'ordre 2:"<<PP2<<endl;
     return 0;
 }
