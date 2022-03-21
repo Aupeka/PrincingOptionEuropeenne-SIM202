8b23fd3665b48d0b795a3ea6a86cd19eefc585c9
Enzod-13 <81633970+Enzod-13@users.noreply.github.com>
2022-03-21 13:18:36 +0100

    Add files via upload

---
+++ Rendu/Affichemaillage.m
@@ -0,0 +1,12 @@
+figure;
+hold on
+
+% maillage
+trimesh(Numtri,Coorneu(:,1),Coorneu(:,2),'Color','k'); 
+view(2);
+axis('equal');
+
+% ajouter eventuellement un titre
+title('Maillage');
+
+hold off;
