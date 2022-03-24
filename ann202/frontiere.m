fae2b05e4a703fe41e00f0d15d5be44e69e12e53
Raph2111 <81633909+Raph2111@users.noreply.github.com>
2022-03-24 16:36:56 +0100

    Update frontiere.m

--- ann202/frontiere.m
+++ ann202/frontiere.m
@@ -1,15 +1,15 @@
 function A = frontiere(x,y)
-% Evalue la valeur de g en un point donné de la frontière
-if x==0 && y==0
+% Evalue la valeur de g en un point donnÃ© de la frontiÃ¨re
+if x==0 && y<=0
     A=0;
 else
-r = sqrt(x^2 + y^2) ;
-if x>=0
-    t = atan(y/x) ;
-    A = r^(2/3)*sin(2*t/3) ;
-else
-    t= pi+atan(y/x);
-    A = r^(2/3)*sin(2*t/3) ;
-end
+    r = sqrt(x^2 + y^2) ;
+    if x>=0
+        t = atan(y/x) ;
+        A = r^(2/3)*sin(2*t/3) ;
+    else
+        t= pi+atan(y/x);
+        A = r^(2/3)*sin(2*t/3) ;
+    end
 end
 end
